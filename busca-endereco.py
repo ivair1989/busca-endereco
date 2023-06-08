8d645c31347f52c7153d216ffaba36438ac73407
Ivair Xisto da Silva <ivair.silva@mitrerealty.com.br>
2023-06-08 18:05:13 -0300

    Buscar endereco

---
+++ busca-endereco.py
@@ -0,0 +1,36 @@
+import requests
+
+#Funcao
+def isnumber(value):
+  try:
+    float(value)
+  except ValueError:
+    return False
+  return True
+
+
+cep = input("Digite o seu cep: ")
+
+#substitui uma string
+cep = cep.replace("-", "")
+
+
+if (isnumber(cep) == False):
+  print("digite um cep valido.")
+else:
+  url = "https://viacep.com.br/ws/" + cep + "/json"
+
+  response = requests.get(url)
+
+  if (response.status_code == 200):
+    result = response.json()
+  
+    print("cep: " + result['cep'])
+    print("logradouro: " + result['logradouro'])
+    print("bairro: " + result['bairro'])
+    print("localidade: " + result['localidade'])
+    print("uf: " + result['uf'])
+  else:
+    print("cep não encontrado")
+    
+input("")
